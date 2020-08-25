09d1fbe4f2b552b8a1d5103681ea5871b5853291
DEVLOG.E10 <DEVLOG.E10@afpar.com>
2020-08-25 15:03:36 +0400

    update

--- LaboM3/METEO/meteo2.js
+++ LaboM3/METEO/meteo2.js
@@ -21,9 +21,14 @@ $(document).ready(function(){
             longitude = position.coords.longitude;
             console.log("longitude = " + longitude +  " latitude = " + latitude);
             // url = `${apiUrl}lat=+${latitude}&lon=${longitude}&appid=${keyApi}&lang=fr&units=metric`;
+
+            //TEST ------------------------------------
+            // latitude = 35.689487;
+            // longitude = 139.691706;
+            //FIN TEST ----------------------------------
            
 
-
+            //je demande à l'api les données requete ajax
             $.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=97c5b5c6988c53e45279eafb74667454&lang=fr&units=metric`,function (data) {
                 console.log(data);
 
@@ -34,7 +39,7 @@ $(document).ready(function(){
 
 
 
-
+                //Modification elements HTML
                 $(".meteoVille").text(data.name);
                 $(".description-weather").text(data.weather[0].description);
                 $(".température").text(temp + "c°");
@@ -51,11 +56,20 @@ $(document).ready(function(){
 
         }, function(){
 
+            $(".meteoVille").text("Indisponnible !");
+            $(".description-weather").text("Vous devez autoriser la géolocalisation pour que le service fonctionne.");
+            // $(".imgMeteo").hide();
+            $(".imgMeteo").attr("src","./iconGeolocationIndisponnible.png").width(150).heigth(150);
+
             console.error("L'utilisateur a refusé la demande de géolocalisation.");
 
         })
 
     }else{
+
+        $(".meteoVille").text("Indisponnible !");
+        $(".description-weather").text("La géolocalisation n'est pas disponnible sur ce navigateur");
+        $(".imgMeteo").attr("src","./iconGeolocationIndisponnible.png").width(150).heigth(150);
 
         console.error("La géolocalisation n'est pas disponnible sur ce navigateur");
     }
