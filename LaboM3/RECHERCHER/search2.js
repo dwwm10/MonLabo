646c03204622290ebb9aa9136317acb9ec03205c
DEVLOG.E10 <DEVLOG.E10@afpar.com>
2020-08-27 09:22:22 +0400

    uptdate files
    implementaion html
    debut JS

---
+++ LaboM3/RECHERCHER/search2.js
@@ -0,0 +1,36 @@
+$(function(){
+
+    
+
+    function getSearchRequest(){
+        
+        //Je recupère la valeur saisie dans zone de recherche....
+        $(".go").on({                                                      //--> Syntaxe pour evenement multiple avec methode .on({...});
+
+            click: function(){
+
+                console.log($(".recherche").val())
+                // var saisie = $("#Recherche").val();
+                // return saisie;
+            }
+
+
+
+
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
